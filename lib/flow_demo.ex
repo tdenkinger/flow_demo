2ea99ff8a1da3b70a74c1991bb1bc181413ac3ae
Troy Denkinger <tdenkinger@gmail.com>
2017-03-18 10:48:05 -0400

    playing with splitting streams

--- lib/flow_demo.ex
+++ lib/flow_demo.ex
@@ -2,24 +2,30 @@ defmodule FlowDemo do
   alias NimbleCSV.RFC4180, as: CSV
 
   def run(file) do
-    File.stream!(file)
-    |> CSV.parse_stream
-    |> Flow.from_enumerable
-    |> Flow.filter(fn(line) -> Enum.at(line, 2) == "Download" end)
-    |> Flow.map(fn(line) -> transform_line(line) end)
-    |> CSV.dump_to_stream
-    |> Stream.into(File.stream!("test/data/downloads.csv"))
-    |> Stream.run
+    sorted_lines =
+    File.read!(file)
+    |> CSV.parse_string
+    |> Enum.reduce(%{download: [], stream: [], not_found: []}, fn(line, breakdown) ->
+         case Enum.at(line, 2) do
+           "Stream" ->
+             Map.update!(breakdown, :stream, &([transform_line(line) | &1]))
+           "Download" ->
+             Map.update!(breakdown, :download, &([transform_line(line) | &1]))
+           _ ->
+             Map.update!(breakdown, :not_found, &([line | &1]))
+         end
+       end)
+
+    File.write("downloads.txt", CSV.dump_to_iodata(sorted_lines[:download]))
+    File.write("streams.txt",   CSV.dump_to_iodata(sorted_lines[:stream]))
   end
 
   defp transform_line(line) do
-    total = calculate_total(Enum.at(line, 3), Enum.at(line, 4))
-
     [
       Enum.at(line, 0),
       Enum.at(line, 3),
       Enum.at(line, 4),
-      total,
+      calculate_total(Enum.at(line, 3), Enum.at(line, 4)),
       Enum.at(line, 1),
     ]
   end
