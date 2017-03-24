9dd82cc728ee072b91bbd9d2d4655d9e974cfd81
Troy Denkinger <tdenkinger@gmail.com>
2017-03-24 06:44:06 -0400

    Switch from Stream to Flow. Not much was done but swap out the library used.

--- lib/flow_demo.ex
+++ lib/flow_demo.ex
@@ -2,16 +2,30 @@ defmodule FlowDemo do
   alias NimbleCSV.RFC4180, as: CSV
 
   def run(file) do
+    files = open_files()
+
     File.stream!(file)
     |> CSV.parse_stream
     |> Flow.from_enumerable
-    |> Flow.filter(fn(line) -> Enum.at(line, 2) == "Download" end)
-    |> Flow.map(fn(line) -> transform_line(line) end)
-    |> CSV.dump_to_stream
-    |> Stream.into(File.stream!("test/data/downloads.csv"))
-    |> Stream.run
+    |> Flow.map(fn(line) -> {file_to_write_to(Enum.at(line, 2), files), prep_line(line)} end)
+    |> Flow.each(fn({file, line}) -> IO.write(file, line) end)
+    |> Flow.run
   end
 
+  defp prep_line(line) do
+    [transform_line(line)] |> CSV.dump_to_iodata
+  end
+
+  defp file_to_write_to("Stream", files),   do: files[:streams]
+  defp file_to_write_to("Download", files), do: files[:downloads]
+  defp file_to_write_to(_, files),          do: files[:notfound]
+
+  defp open_files do
+    %{streams:   File.open!("output/streams.txt",   [:write, :utf8]),
+      downloads: File.open!("output/downloads.txt", [:write, :utf8]),
+      notfound:  File.open!("output/notfound.txt",  [:write, :utf8]),
+    }
+  end
   defp transform_line(line) do
     total = calculate_total(Enum.at(line, 3), Enum.at(line, 4))
 
